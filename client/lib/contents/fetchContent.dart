a36bce9348c95698536655f46a5982bdc7707e39
Guru Dev <guru01803@gmail.com>
2023-12-14 18:14:50 +0530

    update

--- client/lib/contents/fetchContent.dart
+++ client/lib/contents/fetchContent.dart
@@ -1,6 +1,8 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:video_player/video_player.dart';
+import 'package:chewie/chewie.dart';
 
 class FetchContent extends StatefulWidget {
   static const String id = "FetchContent";
@@ -14,6 +16,8 @@ class FetchContent extends StatefulWidget {
 
 class _FetchContentState extends State<FetchContent> {
   List<dynamic> contentArray = [];
+  late VideoPlayerController _videoPlayerController;
+  late ChewieController _chewieController;
 
   @override
   void initState() {
@@ -22,7 +26,7 @@ class _FetchContentState extends State<FetchContent> {
   }
 
   Future<void> fetchData() async {
-    final apiUrl = 'http://192.168.175.79:3000/data/${widget.title}';
+    final apiUrl = 'http://192.168.19.79:3000/data/${widget.title}';
     try {
       final response = await http.get(Uri.parse(apiUrl));
       if (response.statusCode == 200) {
@@ -61,6 +65,8 @@ class _FetchContentState extends State<FetchContent> {
     if (content is String &&
         (content.endsWith('.jpg') || content.endsWith('.png'))) {
       return _buildImage(content);
+    } else if (content is String && content.endsWith('.mp4')) {
+      return _buildVideo(content);
     } else {
       return Center(
         child: Padding(
@@ -79,7 +85,7 @@ class _FetchContentState extends State<FetchContent> {
   }
 
   Widget _buildImage(String imagePath) {
-    imagePath = 'http://192.168.175.79:3000/Images/$imagePath';
+    imagePath = 'http://192.168.19.79:3000/Images/$imagePath';
     print(imagePath);
     return Image.network(
       imagePath,
@@ -87,6 +93,44 @@ class _FetchContentState extends State<FetchContent> {
       height: MediaQuery.of(context).size.height,
       fit: BoxFit.cover,
     );
+  }
+
+  Future<void> _initializeVideoPlayer(String videoPath) async {
+    try {
+      _videoPlayerController = VideoPlayerController.network(videoPath);
+      await _videoPlayerController.initialize();
+    } catch (e) {
+      print('Error initializing video player: $e');
+    }
+  }
+
+  Widget _buildVideo(String videoPath) {
+    videoPath = 'http://192.168.19.79:3000/Videos/$videoPath';
+    print(videoPath);
+
+    _initializeVideoPlayer(videoPath);
+
+    if (_videoPlayerController.value.isInitialized) {
+      _chewieController = ChewieController(
+        videoPlayerController: _videoPlayerController,
+        aspectRatio: 16 / 9,
+        autoInitialize: true,
+        looping: false,
+        autoPlay: false,
+      );
+      return Chewie(controller: _chewieController);
+    } else {
+      return Center(
+        child: Text('Error loading video'),
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    _videoPlayerController.dispose();
+    _chewieController.dispose();
+    super.dispose();
   }
 
   @override
